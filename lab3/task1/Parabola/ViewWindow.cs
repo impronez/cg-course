2f59f453e7a4172678e2d161b017a93b019afe0a
impronez <orzaev7312@gmail.com>
2025-03-04 11:19:02 +0300

    Убрал закомментированные части кода

--- lab3/task1/Parabola/ViewWindow.cs
+++ lab3/task1/Parabola/ViewWindow.cs
@@ -10,7 +10,6 @@ namespace Parabola
         private Shader _shader;
         private Matrix4 _projection;
 
-        //private ParabolaArgs _args;
         private Graph _graph;
 
         public ViewWindow(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings, GraphArgs args) 
@@ -28,100 +27,11 @@ namespace Parabola
             GL.Clear(ClearBufferMask.ColorBufferBit);
 
             _shader.Use();
-            //UpdateVertexFormat();
-
-            //DrawCoordinateAxes();
-
-            //DrawParabola();
             _graph.Render();
 
             SwapBuffers();
 
             base.OnRenderFrame(args);
-        }
-
-        private void DrawParabola()
-        {
-            //var vertices = Parabola.GetVertices(_args.Function, _args.MinValue, _args.MaxValue, _args.Color);
-
-            ////float[] floatArray = vertices.SelectMany(v => Vertex.ToFloatArray(v)).ToArray();
-
-
-            //AddArrayToArrayBuffer(vertices.ToArray());
-            //GL.DrawArrays(PrimitiveType.LineStrip, 0, vertices.Count / 6);
-        }
-
-        private void DrawCoordinateAxes()
-        {
-            float color = 0.5f;
-            float colorR = color;
-            float colorG = color;
-            float colorB = color;
-
-            List<float> axesList = new()
-            {
-                0f, -1f, 0f, colorR, colorG, colorB,
-                0f, 1f, 0f, colorR, colorG, colorB,
-                -1f, 0f, 0f, colorR, colorG, colorB,
-                1f, 0f, 0f, colorR, colorG, colorB,
-            };
-
-            AddStrokesToAxes(axesList, colorR, colorG, colorB);
-            AddArrowsToAxes(axesList, colorR, colorG, colorB);
-
-            AddArrayToArrayBuffer(axesList.ToArray());
-            GL.DrawArrays(PrimitiveType.Lines, 0, axesList.Count / 6);
-        }
-
-        private void AddArrowsToAxes(List<float> axesList, float colorR, float colorG, float colorB)
-        {
-            float width = 0.02f;
-            float indent = 0.03f;
-
-            List<float> arrowsList = new()
-            {
-                -width, 1 - indent, 0f, colorR, colorG, colorB,
-                0f, 1f, 0f, colorR, colorG, colorB,
-                width, 1 - indent, 0f, colorR, colorG, colorB,
-                0f, 1f, 0f, colorR, colorG, colorB,
-
-                1 - indent, width, 0f, colorR, colorG, colorB,
-                1f, 0f, 0f, colorR, colorG, colorB,
-                1 - indent, -width, 0f, colorR, colorG, colorB,
-                1f, 0f, 0f, colorR, colorG, colorB,
-            };
-
-            axesList.AddRange(arrowsList);
-        }
-
-        private void AddStrokesToAxes(List<float> axesList, float colorR, float colorG, float colorB)
-        {
-            float strokeWidth = 0.01f;
-
-            for (float i = -0.9f; i < 1.0f;  i += 0.1f)
-            {
-                axesList.AddRange([i, strokeWidth, 0.0f, colorR, colorG, colorB]);
-                axesList.AddRange([i, -strokeWidth, 0.0f, colorR, colorG, colorB]);
-
-                axesList.AddRange([strokeWidth, i, 0.0f, colorR, colorG, colorB]);
-                axesList.AddRange([-strokeWidth, i, 0.0f, colorR, colorG, colorB]);
-            }
-        }
-
-        private void AddArrayToArrayBuffer(float[] array)
-        {
-            GL.BufferData(BufferTarget.ArrayBuffer, array.Length * sizeof(float), array, BufferUsageHint.StaticDraw);
-        }
-
-        private void UpdateVertexFormat()
-        {
-            // Вершины
-            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 0);
-            GL.EnableVertexAttribArray(0);
-            // Цвет
-            // Сделать так, чтобы в одном месте была отрисовка и применение 6 и 3
-            GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 3 * sizeof(float));
-            GL.EnableVertexAttribArray(1);
         }
 
         private void UpdateOrthographicMatrix()
