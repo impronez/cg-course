49993a0d3dedba1c44e58befcf384c4a1e3723c9
impronez <orzaev7312@gmail.com>
2025-03-18 17:27:43 +0300

    Создал класс RGB-вершины

---
+++ lab4/MobiusStrip/Utilities/RGBAVertex.cs
@@ -0,0 +1,30 @@
+using OpenTK.Mathematics;
+
+namespace MobiusStrip.Utilities;
+
+public struct RGBAVertex
+{
+    public const int Size = 7;
+    public const int ColorIndex = 3;
+
+    public readonly Vector3 Position;
+    
+    public readonly Color4 Color;
+
+    public RGBAVertex(Vector3 position, Color4 color = default)
+    {
+        Position = position;
+        Color = color == default ? Color4.Black : color;
+    }
+    
+    public RGBAVertex(float x, float y, float z, Color4 color = default)
+    {
+        Position = new Vector3(x, y, z);
+        Color = color == default ? Color4.Black : color;
+    }
+
+    public float[] ToArray()
+    {
+        return [Position.X, Position.Y, Position.Z, Color.R, Color.G, Color.B, Color.A];
+    }
+}
