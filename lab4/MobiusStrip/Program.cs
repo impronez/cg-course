592704952438847738761f685da213428c7e36e0
impronez <orzaev7312@gmail.com>
2025-03-18 17:28:34 +0300

    Создал проект

---
+++ lab4/MobiusStrip/Program.cs
@@ -0,0 +1,24 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace MobiusStrip;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var nativeWindowSettings = new NativeWindowSettings()
+        {
+            ClientSize = new Vector2i(800, 600),
+            Title = "Mobius Strip",
+            
+            Flags = ContextFlags.Default,
+            APIVersion = new Version(3, 3),
+            Profile = ContextProfile.Compatability
+        };
+
+        using var window = new ViewWindow(GameWindowSettings.Default, nativeWindowSettings);
+        window.Run();
+    }
+}
