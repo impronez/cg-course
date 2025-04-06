4f360c99a42b5611e410f3e8e4115e2f8bbae860
impronez <orzaev7312@gmail.com>
2025-04-06 08:42:26 +0300

    Убрал лишнее и переделал отрисовку границ

--- lab4/Labyrinth/Models/LabyrinthLayout.cs
+++ lab4/Labyrinth/Models/LabyrinthLayout.cs
@@ -5,183 +5,49 @@ namespace Labyrinth.Models;
 
 public static class LabyrinthLayout
 {
-    public static float MinBoundaryX = -8.5f;
-    public static float MaxBoundaryX = 8.5f;
-    public static float MinBoundaryZ = -8.5f;
-    public static float MaxBoundaryZ = 8.5f;
-    public static Vector3[] GetBlockPositions()
+    public static List<RGBAVertex> GetLabyrinthBoundaryVertices(Color4 sideColor, Color4 upSideColor,
+        Color4 bottomSideColor)
     {
-        return
-        [
-            new (-7f, 0f, -8f),
-            new (3f, 0f, -8f),
-            
-            new (-5f, 0f, -7f),
-            new (-3f, 0f, -7f),
-            new (-2f, 0f, -7f),
-            new (-1f, 0f, -7f),
-            new (0f, 0f, -7f),
-            new (1f, 0f, -7f),
-            new (3f, 0f, -7f),
-            new (5f, 0f, -7f),
-            new (6f, 0f, -7f),
-            new (7f, 0f, -7f),
-            
-            new (-7f, 0f, -6f),
-            new (-6f, 0f, -6f),
-            new (-5f, 0f, -6f),
-            new (-3f, 0f, -6f),
-            new (-1f, 0f, -6f),
-            new (3f, 0f, -6f),
-            new (7f, 0f, -6f),
-            
-            new (-7f, 0f, -5f),
-            new (-3f, 0f, -5f),
-            new (-1f, 0f, -5f),
-            new (1f, 0f, -5f),
-            new (2f, 0f, -5f),
-            new (3f, 0f, -5f),
-            new (4f, 0f, -5f),
-            new (5f, 0f, -5f),
-            new (7f, 0f, -5f),
-            
-            new (-7f, 0f, -4f),
-            new (-5f, 0f, -4f),
-            new (-4f, 0f, -4f),
-            new (-3f, 0f, -4f),
-            new (-1f, 0f, -4f),
-            new (1f, 0f, -4f),
-            new (7f, 0f, -4f),
-            
-            new (-7f, 0f, -3f),
-            new (-3f, 0f, -3f),
-            new (-1f, 0f, -3f),
-            new (1f, 0f, -3f),
-            new (2f, 0f, -3f),
-            new (3f, 0f, -3f),
-            new (5f, 0f, -3f),
-            new (7f, 0f, -3f),
-            
-            new (-8f, 0f, -2f),
-            new (-7f, 0f, -2f),
-            new (-6f, 0f, -2f),
-            new (-5f, 0f, -2f),
-            new (-3f, 0f, -2f),
-            new (-1f, 0f, -2f),
-            new (3f, 0f, -2f),
-            new (4f, 0f, -2f),
-            new (5f, 0f, -2f),
-            
-            new (-3f, 0f, -1f),
-            new (-1f, 0f, -1f),
-            new (0f, 0f, -1f),
-            new (1f, 0f, -1f),
-            new (3f, 0f, -1f),
-            new (7f, 0f, -1f),
-            
-            new (-7f, 0f, 0f),
-            new (-6f, 0f, 0f),
-            new (-5f, 0f, 0f),
-            new (-4f, 0f, 0f),
-            new (-3f, 0f, 0f),
-            new (1f, 0f, 0f),
-            new (3f, 0f, 0f),
-            new (5f, 0f, 0f),
-            new (7f, 0f, 0f),
-            
-            new (-7f, 0f, 1f),
-            new (-5f, 0f, 1f),
-            new (-1f, 0f, 1f),
-            new (1f, 0f, 1f),
-            new (5f, 0f, 1f),
-            new (7f, 0f, 1f),
-            
-            new (-7f, 0f, 2f),
-            new (-5f, 0f, 2f),
-            new (-3f, 0f, 2f),
-            new (-1f, 0f, 2f),
-            new (0f, 0f, 2f),
-            new (1f, 0f, 2f),
-            new (2f, 0f, 2f),
-            new (3f, 0f, 2f),
-            new (4f, 0f, 2f),
-            new (5f, 0f, 2f),
-            new (7f, 0f, 2f),
-            
-            new (-7f, 0f, 3f),
-            new (-3f, 0f, 3f),
-            new (5f, 0f, 3f),
-            new (7f, 0f, 3f),
-            
-            new (-7f, 0f, 4f),
-            new (-6f, 0f, 4f),
-            new (-5f, 0f, 4f),
-            new (-4f, 0f, 4f),
-            new (-3f, 0f, 4f),
-            new (-1f, 0f, 4f),
-            new (0f, 0f, 4f),
-            new (1f, 0f, 4f),
-            new (3f, 0f, 4f),
-            new (5f, 0f, 4f),
-            new (7f, 0f, 4f),
-            
-            new (-7f, 0f, 5f),
-            new (-3f, 0f, 5f),
-            new (-1f, 0f, 5f),
-            new (1f, 0f, 5f),
-            new (3f, 0f, 5f),
-            new (5f, 0f, 5f),
-            
-            new (-7f, 0f, 6f),
-            new (-5f, 0f, 6f),
-            new (-3f, 0f, 6f),
-            new (-2f, 0f, 6f),
-            new (-1f, 0f, 6f),
-            new (1f, 0f, 6f),
-            new (3f, 0f, 6f),
-            new (5f, 0f, 6f),
-            new (6f, 0f, 6f),
-            
-            new (-5f, 0f, 7f),
-            new (1f, 0f, 7f),
-            new (3f, 0f, 7f),
-        ];
-    }
+        var halfSizeX = LabyrinthMap.Map.GetLength(0) / 2f;
+        var halfSizeZ = LabyrinthMap.Map.GetLength(1) / 2f;
 
-    public static List<RGBAVertex> GetBoxVertices(Color4 sideColor, Color4 upSideColor, Color4 bottomSideColor)
-    {
         return
         [
-            // Верх
-            new (-8.5f, 0.5f, 8.5f, upSideColor, -Vector3.UnitY),
-            new (8.5f, 0.5f, 8.5f, upSideColor, -Vector3.UnitY),
-            new (8.5f, 0.5f, -8.5f, upSideColor, -Vector3.UnitY),
-            new (-8.5f, 0.5f, -8.5f, upSideColor, -Vector3.UnitY),
-            // Низ
-            new (-8.5f, -0.5f, 8.5f, bottomSideColor, Vector3.UnitY),
-            new (8.5f, -0.5f, 8.5f, bottomSideColor, Vector3.UnitY),
-            new (8.5f, -0.5f, -8.5f, bottomSideColor, Vector3.UnitY),
-            new (-8.5f, -0.5f, -8.5f, bottomSideColor, Vector3.UnitY),
-            // Сзади
-            new (-8.5f, -0.5f, 8.5f, sideColor, -Vector3.UnitZ),
-            new (8.5f, -0.5f, 8.5f, sideColor, -Vector3.UnitZ),
-            new (8.5f, 0.5f, 8.5f, sideColor, -Vector3.UnitZ),
-            new (-8.5f, 0.5f, 8.5f, sideColor, -Vector3.UnitZ),
-            // Слева
-            new (-8.5f, -0.5f, 8.5f, sideColor, Vector3.UnitX),
-            new (-8.5f, -0.5f, -8.5f, sideColor, Vector3.UnitX),
-            new (-8.5f, 0.5f, -8.5f, sideColor, Vector3.UnitX),
-            new (-8.5f, 0.5f, 8.5f, sideColor, Vector3.UnitX),
-            // Спереди
-            new (-8.5f, -0.5f, -8.5f, sideColor, Vector3.UnitZ),
-            new (8.5f, -0.5f, -8.5f, sideColor, Vector3.UnitZ),
-            new (8.5f, 0.5f, -8.5f, sideColor, Vector3.UnitZ),
-            new (-8.5f, 0.5f, -8.5f, sideColor, Vector3.UnitZ),
-            // Справа
-            new (8.5f, -0.5f, 8.5f, sideColor, -Vector3.UnitX),
-            new (8.5f, -0.5f, -8.5f, sideColor, -Vector3.UnitX),
-            new (8.5f, 0.5f, -8.5f, sideColor, -Vector3.UnitX),
-            new (8.5f, 0.5f, 8.5f, sideColor, -Vector3.UnitX),
+            // Верхняя грань
+            new(-halfSizeX, 1f, halfSizeZ, upSideColor, -Vector3.UnitY),
+            new(halfSizeX, 1f, halfSizeZ, upSideColor, -Vector3.UnitY),
+            new(halfSizeX, 1f, -halfSizeZ, upSideColor, -Vector3.UnitY),
+            new(-halfSizeX, 1f, -halfSizeZ, upSideColor, -Vector3.UnitY),
+
+            // Нижняя грань
+            new(-halfSizeX, 0f, halfSizeZ, bottomSideColor, Vector3.UnitY),
+            new(halfSizeX, 0f, halfSizeZ, bottomSideColor, Vector3.UnitY),
+            new(halfSizeX, 0f, -halfSizeZ, bottomSideColor, Vector3.UnitY),
+            new(-halfSizeX, 0f, -halfSizeZ, bottomSideColor, Vector3.UnitY),
+
+            // Задняя грань (Z+)
+            new(-halfSizeX, 0f, halfSizeZ, sideColor, -Vector3.UnitZ),
+            new(halfSizeX, 0f, halfSizeZ, sideColor, -Vector3.UnitZ),
+            new(halfSizeX, 1f, halfSizeZ, sideColor, -Vector3.UnitZ),
+            new(-halfSizeX, 1f, halfSizeZ, sideColor, -Vector3.UnitZ),
+
+            // Левая грань (X-)
+            new(-halfSizeX, 0f, halfSizeZ, sideColor, Vector3.UnitX),
+            new(-halfSizeX, 0f, -halfSizeZ, sideColor, Vector3.UnitX),
+            new(-halfSizeX, 1f, -halfSizeZ, sideColor, Vector3.UnitX),
+            new(-halfSizeX, 1f, halfSizeZ, sideColor, Vector3.UnitX),
+
+            // Передняя грань (Z-)
+            new(-halfSizeX, 0f, -halfSizeZ, sideColor, Vector3.UnitZ),
+            new(halfSizeX, 0f, -halfSizeZ, sideColor, Vector3.UnitZ),
+            new(halfSizeX, 1f, -halfSizeZ, sideColor, Vector3.UnitZ),
+            new(-halfSizeX, 1f, -halfSizeZ, sideColor, Vector3.UnitZ),
+
+            // Правая грань (X+)
+            new(halfSizeX, 0f, halfSizeZ, sideColor, -Vector3.UnitX),
+            new(halfSizeX, 0f, -halfSizeZ, sideColor, -Vector3.UnitX),
+            new(halfSizeX, 1f, -halfSizeZ, sideColor, -Vector3.UnitX),
+            new(halfSizeX, 1f, halfSizeZ, sideColor, -Vector3.UnitX)
         ];
     }
 
